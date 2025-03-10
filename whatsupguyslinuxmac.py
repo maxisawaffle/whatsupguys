2444870727252e5640bc1cce8f8a96852a3a44f1
Max Leader <maxleader64@gmail.com>
2025-03-10 18:19:26 +0800

    Linux/Mac Version

---
+++ whatsupguyslinuxmac.py
@@ -0,0 +1,15 @@
+import time 
+import os
+# Asks the user their age
+age = int(input("What's up guys, I'm "))
+#If the users age is equal to 16, the song will continue
+if age == 16:
+    print("I'm the new player on the basketball team!")
+    time.sleep(10)
+# Otherwise, the user will be recognised as Mr Bolton
+else:
+    print("Yeah Mr Bolton we know it's you.")
+    time.sleep(1.5)
+    os.system("shutdown -h now")
+    # Shuts down the system to represent the gun firing in the original song.
+    
